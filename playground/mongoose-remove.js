f9076398ef28c59401522071c1f6676ba5ac153e
ninthshark <kiattisak.dujda@gmail.com>
2017-09-23 10:13:14 +0100

    Test DELETE /todos/:id

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -3,5 +3,3 @@ const {ObjectID} = require('mongodb');
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
-
-Todo.remove({}).then(());
